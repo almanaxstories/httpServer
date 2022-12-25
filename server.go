33e5725b9148f3bd621c29b5c2d228f4421bff0b
Arcantar <almanaxstories@gmail.com>
2022-12-25 12:18:35 +0200

    POST catched successfully

--- server.go
+++ server.go
@@ -6,9 +6,10 @@ import (
 	"net/http"
 )
 
+var fileServer = http.FileServer(http.Dir("./assets"))
+
 func main() {
-	fileServer := http.FileServer(http.Dir("src/"))
-	http.Handle("/src", fileServer)
+
 	http.HandleFunc("/", serveIndexPage)
 
 	if err := http.ListenAndServe(":8090", nil); err != nil {
@@ -17,25 +18,37 @@ func main() {
 }
 
 func serveIndexPage(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "404 Not Found", http.StatusNotFound)
+	log.Printf("method %s, path: %s\n", r.Method, r.URL.Path)
+	if r.Method == http.MethodGet {
+		fileServer.ServeHTTP(w, r)
 		return
-	}
-	switch r.Method {
-	case "GET":
-		http.ServeFile(w, r, "index.html")
-	case "POST":
+	} else if r.Method == http.MethodPost {
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
-		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
-		name := r.FormValue("name")
-		address := r.FormValue("address")
-		fmt.Fprintf(w, "Name = %s\n", name)
-		fmt.Fprintf(w, "Address = %s\n", address)
-	default:
-		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+
+		fmt.Println("POST catched successfully!")
+		/*fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+		firstName := r.FormValue("fNameText")
+		lastName := r.FormValue("lNameText")
+		//test := r.FormValue()
+		fmt.Fprintf(w, "Name = %s\n", firstName)
+		fmt.Fprintf(w, "Address = %s\n", lastName)*/
 	}
+
+	//case "POST":
+	//  if err := r.ParseForm(); err != nil {
+	//    fmt.Fprintf(w, "ParseForm() err: %v", err)
+	//    return
+	//  }
+	//  fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+	//  name := r.FormValue("name")
+	//  address := r.FormValue("address")
+	//  fmt.Fprintf(w, "Name = %s\n", name)
+	//  fmt.Fprintf(w, "Address = %s\n", address)
+	//default:
+	//  fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	//}
 
 }
