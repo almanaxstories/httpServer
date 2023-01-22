6d0c6eefb1a9354e48edacc5a9e8d4579839599e
Arcantar <almanaxstories@gmail.com>
2023-01-22 19:01:41 +0200

    When user enters his data server inserts it into the db's table and selects * from it to server's console

--- server.go
+++ server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 var fileServer = http.FileServer(http.Dir("./assets"))
@@ -44,7 +48,58 @@ func serveIndexPage(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Println("User's first name is:", result["fNameText"])
 		fmt.Println("User's last name is:", result["lNameText"])
-		//fileServer.ServeHTTP(w, r)
 
+		//os.Getenv("DATABASE_URL")
+		dbURL := "postgres://arcantar:2244foxtrot@localhost:5432/arcantar"
+		db, err := sql.Open("pgx", dbURL)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		} else {
+			fmt.Println("Successfully connected to DB")
+		}
+
+		insertUserIntoDB(db, fmt.Sprint(result["fNameText"]), fmt.Sprint(result["lNameText"]))
+		getUsersInfoFromDB(db)
+		db.Close()
 	}
+}
+
+func getUsersInfoFromDB(db *sql.DB) {
+	var (
+		uid    int
+		ufname string
+		ulname string
+	)
+
+	rows, err := db.Query("SELECT * from usersinfo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		err := rows.Scan(&uid, &ufname, &ulname)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(uid, ufname, ulname)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows.Close()
+}
+
+func insertUserIntoDB(db *sql.DB, ufname string, ulname string) {
+	insert, insertErr := db.Prepare("INSERT INTO usersinfo (ufname, ulname) VALUES ($1, $2)")
+	if insertErr != nil {
+		log.Fatal(insertErr)
+	}
+	_, err := insert.Exec(ufname, ulname)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("You have successfully added new user's info to database!")
+	}
+	insert.Close()
 }
